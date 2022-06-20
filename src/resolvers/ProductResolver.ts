4a4bfe02e9d063ed8075dc704c75e3039a8a8bd6
Lucas Chavez <luck9217@gmail.com>
2022-06-20 22:17:47 +0200

    feat:mutation delete upload created

--- src/resolvers/ProductResolver.ts
+++ src/resolvers/ProductResolver.ts
@@ -1,6 +1,13 @@
-import { Resolver, Query, Mutation, Arg, InputType, Field, Int } from "type-graphql";
-
-import { Product } from "../entity/Products";
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  Int,
+  Query,
+  InputType,
+  Field
+} from "type-graphql";
+import { Product } from "../entity/Product";
 
 @InputType()
 class ProductInput {
@@ -11,22 +18,43 @@ class ProductInput {
   quantity!: number;
 }
 
+@InputType()
+class ProductUpdateInput {
+  @Field(() => String, {nullable: true})
+  name?: string;// ? and nullable:true  optional data
+
+  @Field(() => Int, {nullable: true})
+  quantity?: number;
+}
+
 @Resolver()
 export class ProductResolver {
-  @Mutation(() => Boolean)
+  @Mutation(() => Product)
   async createProduct(
-    @Arg("name") name: string,
-    @Arg("quantity") quantity: number
+    // @Arg("name") name: string,
+    // @Arg("quantity", () => Int) quantity: number
+    @Arg("variables", () => ProductInput) variables: ProductInput
   ) {
-    const result = Product.insert({ name, quantity });
+    const newProduct = await Product.insert(variables);
+    return variables;
+  }
 
-
-    console.log(result);
-
+  @Mutation(() => Boolean)
+  async deleteProduct(@Arg("id", () => Int) id: number) {
+    await Product.delete(id);
     return true;
   }
 
-  @Query(() => [Product])// add @field to entity to relationaty input on query
+  @Mutation(() => Boolean)
+  async updateProduct(
+    @Arg("id", () => Int) id: number,
+    @Arg("fields", () => ProductUpdateInput) fields: ProductUpdateInput
+  ) {
+    await Product.update({ id }, fields);
+    return true;
+  }
+
+  @Query(() => [Product])// @Field on entity to traslate to field query
   products() {
     return Product.find();
   }
